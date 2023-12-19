f3ccd983498622d6eaf9266e35bdc86c7c888c3b
Esphend <145009934+Esphend@users.noreply.github.com>
2023-12-20 00:02:59 +0500

    Update gen_3L.py

--- gen_3L.py
+++ gen_3L.py
@@ -4,23 +4,23 @@ def generate_3l(input_str_3l):
     enternum = 0
     if enternum != 0:
         searchstr += "|"
-    if element[0] == element[1] == element[2]:                                             # [0] = [1] = [2]
+    if element[0] == element[1] == element[2]:                                          
         searchstr += element[0] + "-" + element[1] + "-" + element[2]
         searchstr += "|"
-    if element[0] != element[1] and element[0] != element[2] and element[1] != element[2]: # [0] != [1] != [2]
+    if element[0] != element[1] and element[0] != element[2] and element[1] != element[2]: 
         searchstr += r'([^w])-(?!\1|w)(.)(-.)*-(?!\1|\2|w)(.)'
         searchstr += "|"
     if not (element[0] == element[1] == element[2]):
-            if element[0] == element[1]:                             # [0] = [1]
+            if element[0] == element[1]:                           
                     searchstr += element[0] + "-" + element[0] + "-" + element[2] + "|"
                     searchstr += element[0] + "-" + element[2] + "-" + element[0] + "|"
                     searchstr += element[2] + "-" + element[0] + "-" + element[0] + "|"
-            if element[0] == element[2]:                             # [0] = [2]
+            if element[0] == element[2]:                         
                     searchstr += element[0] + "-" + element[0] + "-" + element[1] + "|"
                     searchstr += element[0] + "-" + element[1] + "-" + element[0] + "|"
                     searchstr += element[1] + "-" + element[0] + "-" + element[0] + "|"
 
-            if element[1] == element[2]:                             # [1] = [2]
+            if element[1] == element[2]:                        
                     searchstr += element[1] + "-" + element[1] + "-" + element[0] + "|"
                     searchstr += element[1] + "-" + element[0] + "-" + element[1] + "|"
                     searchstr += element[0] + "-" + element[1] + "-" + element[1] + "|"
